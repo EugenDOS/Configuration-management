d1c24cf4674f6b92b6f804a84369a90964d1886d
EugenDOS <emchernetsov@gmail.com>
2024-09-20 22:14:57 +0300

    Команды терминала вынесены в отдельные функции; добавлена функция очистки поля вывода `clear` с кнопкой её задействования

--- Домашние задания/Задание №1/task_1.py
+++ Домашние задания/Задание №1/task_1.py
@@ -12,33 +12,52 @@ args = parser.parse_args()
 
 def command():
     command = input_area.get("1.0", tk.END)[:-1]
-
+    
     with ZipFile(args.zip_path, "a") as myzip:
-        if command == "ls":
-            names = ""
-            for name in myzip.namelist():
-                names += name + "\n"
-            output_area.insert(tk.END, names)
-
-        elif command == "exit":
-            exit()
-
-        elif command.startswith("cat"):
+        
+        if command == "ls": ls(myzip.namelist())
+        
+        elif command == "exit": exit()
+        
+        elif command.startswith("cat"): 
             path = command.split()[1]
-            content = myzip.read(path).decode()
-            output_area.insert(tk.END, content + "\n")
+            cat(myzip.read(path).decode())
+
+            
+def write(text_widget: tk.Text, text):
+    text_widget.configure(state=tk.NORMAL)
+    text_widget.insert(tk.END, text)
+    text_widget.configure(state=tk.DISABLED)
+
+def ls(name_list):
+    names = ""
+    for name in name_list:
+        names += name + "\n"
+    write(output_area, names)
+
+def cat(content):
+    output_area.insert(tk.END, content + "\n")
+
+def clear():
+    output_area.delete('1.0', tk.END)
 
 
 root = tk.Tk()
-root.title("Linux command prompt visializer")
+root.title(f"Terminal — {args.user_name}@{args.pc_name}")
 
-output_area = tk.Text(root, height=10, width=40)
+output_area = tk.Text(root, height=10, width=45, state=tk.DISABLED)
 output_area.pack(pady=10)
 
-input_area = tk.Text(root, height=2, width=40)
+input_area = tk.Text(root, height=2, width=45)
 input_area.pack(pady=10)
 
-exec_button = tk.Button(root, text="Run", command=command)
-exec_button.pack(pady=5)
+button_frame = tk.Frame(root)
+button_frame.pack(pady=10)
+
+exec_button = tk.Button(button_frame, text="Run", command=command)
+exec_button.pack(side=tk.LEFT, padx=10)
+
+clear_button = tk.Button(button_frame, text="Clear", command=clear)
+clear_button.pack(side=tk.LEFT, padx=10)
 
 root.mainloop()
